dd6a962a9819b76dff25e2c7e74eeba553d23f69
Marcelo Zometa Paniagua <marcelo.zometapaniag@ad.snow.edu>
2020-08-28 11:44:57 -0600

    Added main form

---
+++ Calculator.WinForms/Main_Form.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Calculator.WinForms
+{
+    public partial class Main_Form : Form
+    {
+        public Main_Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
