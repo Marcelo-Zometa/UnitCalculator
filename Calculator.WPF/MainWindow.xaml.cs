49edba9572724f6b27d09c89983064b62646c378
Marcelo Zometa <marcelo.zometa@hotmail.com>
2020-09-05 11:21:20 -0600

    combo box working

--- Calculator.WPF/MainWindow.xaml.cs
+++ Calculator.WPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Calculator.WPF.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,12 @@ namespace Calculator.WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        MainWindowViewModel vm;
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = vm = new MainWindowViewModel();
         }
     }
 }
---
+++ Calculator.WPF/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Transactions;
+
+namespace Calculator.WPF.ViewModel
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public Unit calculator { get; set; }
+
+        public MainWindowViewModel()
+        {
+            OldUnitList = new ObservableCollection<string>()
+            {
+                "cm", "ft", "in", "km", "m", "mile"
+            };
+
+            NewUnitList = new ObservableCollection<string>()
+            {
+                "cm", "ft", "in", "km", "m", "mile"
+            };
+
+        }
+
+        private ObservableCollection<string> _oldUnitList;
+
+        public ObservableCollection<string> OldUnitList
+        {
+            get { return _oldUnitList; }
+            set { _oldUnitList = value; }
+        }
+
+        private ObservableCollection<string> _newUnitList;
+
+        public ObservableCollection<string> NewUnitList
+        {
+            get { return _newUnitList; }
+            set { _newUnitList = value; }
+        }
+    }
+}
